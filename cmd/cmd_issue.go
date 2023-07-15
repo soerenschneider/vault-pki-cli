d72d59f95fbdb56a3d159ce3146688cd367470b9
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-07-15 14:53:42 +0100

    refactor: small refactor

--- cmd/cmd_issue.go
+++ cmd/cmd_issue.go
@@ -22,7 +22,6 @@ import (
 	"github.com/soerenschneider/vault-pki-cli/pkg"
 	"github.com/soerenschneider/vault-pki-cli/pkg/issue_strategies"
 
-	"github.com/hashicorp/vault/api"
 	log "github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
@@ -61,11 +60,12 @@ func getIssueCmd() *cobra.Command {
 
 func issueCertEntryPoint(_ *cobra.Command, _ []string) {
 	PrintVersionInfo()
-
 	config, err := config()
 	DieOnErr(err, "could not get config")
-
 	config.Print()
+
+	err = config.ValidateIssue()
+	DieOnErr(err, "invalid config")
 
 	if config.Daemonize && len(config.MetricsAddr) > 0 {
 		log.Info().Msgf("Starting metrics server at '%s'", config.MetricsAddr)
@@ -75,9 +75,6 @@ func issueCertEntryPoint(_ *cobra.Command, _ []string) {
 		}()
 	}
 
-	err = config.ValidateIssue()
-	DieOnErr(err, "invalid config")
-
 	ticker := time.NewTicker(tickDuration)
 	defer ticker.Stop()
 
@@ -85,9 +82,9 @@ func issueCertEntryPoint(_ *cobra.Command, _ []string) {
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	done := make(chan bool, 1)
 
+	pkiImpl, sink := buildDependencies(config)
 	for {
-		storage.InitBuilder(config)
-		err = issueCert(config)
+		err = issueCert(config, pkiImpl, sink)
 		if err != nil {
 			log.Error().Err(err).Msg("issuing cert not successful")
 			internal.MetricSuccess.WithLabelValues(config.CommonName).Set(0)
@@ -121,8 +118,18 @@ func issueCertEntryPoint(_ *cobra.Command, _ []string) {
 	}
 }
 
-func issueCert(config *conf.Config) error {
-	vaultClient, err := api.NewClient(getVaultConfig(config))
+func buildRenewalStrategy(config *conf.Config) (issue_strategies.IssueStrategy, error) {
+	if config.ForceNewCertificate {
+		return &issue_strategies.StaticRenewal{Decision: true}, nil
+	}
+
+	return issue_strategies.NewPercentage(config.CertificateLifetimeThresholdPercentage)
+}
+
+func buildDependencies(config *conf.Config) (*pki.PkiCli, pki.IssueSink) {
+	storage.InitBuilder(config)
+
+	vaultClient, err := buildVaultClient(config)
 	DieOnErr(err, "can't build client")
 
 	authStrategy, err := buildAuthImpl(vaultClient, config)
@@ -131,15 +138,8 @@ func issueCert(config *conf.Config) error {
 	vaultBackend, err := vault.NewVaultPki(vaultClient, authStrategy, config)
 	DieOnErr(err, "can't build vault pki")
 
-	var strat issue_strategies.IssueStrategy
-	if config.ForceNewCertificate {
-		strat = &issue_strategies.StaticRenewal{Decision: true}
-	} else {
-		strat, err = issue_strategies.NewPercentage(config.CertificateLifetimeThresholdPercentage)
-		if err != nil {
-			return err
-		}
-	}
+	strat, err := buildRenewalStrategy(config)
+	DieOnErr(err, "can't build renewal strategy")
 
 	pkiImpl, err := pki.NewPki(vaultBackend, strat)
 	DieOnErr(err, "can't build pki impl")
@@ -147,6 +147,10 @@ func issueCert(config *conf.Config) error {
 	sink, err := sink.MultiKeyPairSinkFromConfig(config)
 	DieOnErr(err, "can't build sink")
 
+	return pkiImpl, sink
+}
+
+func issueCert(config *conf.Config, pkiImpl *pki.PkiCli, sink pki.IssueSink) error {
 	var serial string
 	x509cert, err := sink.ReadCert()
 	if err == nil {
@@ -165,6 +169,12 @@ func issueCert(config *conf.Config) error {
 		}
 	}
 
+	tidyStorage(pkiImpl)
+
+	return err
+}
+
+func tidyStorage(pkiImpl *pki.PkiCli) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano())) // #nosec G404
 	if r.Intn(100) >= 90 {
 		log.Info().Msgf("Tidying up certificate storage")
@@ -173,8 +183,6 @@ func issueCert(config *conf.Config) error {
 			log.Error().Msgf("Tidying up certificate storage failed: %v", err)
 		}
 	}
-
-	return err
 }
 
 func runPostIssueHooks(config *conf.Config) error {
